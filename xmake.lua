54d22b001a4b51ce8becde40907a02663372ee95
kuangby <3091240752@qq.com>
2025-07-21 19:17:28 +0800

    adapt to 1.21.80

--- xmake.lua
+++ xmake.lua
@@ -14,8 +14,8 @@ add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 -- add_requires("levibuildscript")
 
 add_requires(
-    "levilamina 1.2.0",
-    "levibuildscript 0.4.0"
+    "levilamina 1.3.4",
+    "levibuildscript"
 )
 
 if not has_config("vs_runtime") then
